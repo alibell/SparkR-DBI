8a568a8219c97ffd95cff3fef17c2b1319fdcfa9
Ali Bellamine <contact@alibellamine.me>
2022-10-20 20:52:05 +0200

    Update dbi test

--- tests/testthat/test-dbi.R
+++ tests/testthat/test-dbi.R
@@ -6,5 +6,6 @@ test_that("dbSendQuery produce a SparkResult object", {
     sdf <- SparkR::as.DataFrame(df)
     SparkR::saveAsTable(sdf, "testTable", overwrite=T)
     res <- dbSendQuery(conn, "SELECT * FROM testTable")
+    message(class(res))
     expect_identical(class(res), "SparkResult")
 })
