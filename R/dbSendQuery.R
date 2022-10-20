020405566ecc76b5217c99df1d5686e5e92b50ae
Ali Bellamine <contact@alibellamine.me>
2022-10-20 20:43:09 +0200

    Update dbSendQuery

--- R/dbSendQuery.R
+++ R/dbSendQuery.R
@@ -3,7 +3,7 @@
 #' @export
 setMethod("dbSendQuery", "SparkRConnection", function(conn, statement, ...) {
   # Executing query
-  state <- env(start=1, end=-1, completed=FALSE, cleared=FALSE, df=NULL, statement=statement)
+  state <- rlang::env(start=1, end=-1, completed=FALSE, cleared=FALSE, df=NULL, statement=statement)
 
   new("SparkRResult", statement=statement, state=state, ...)
 })
