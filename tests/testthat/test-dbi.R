fc99129fc9a0739b470a3968d107c19bd6455c13
Ali Bellamine <contact@alibellamine.me>
2022-10-20 20:38:18 +0200

    update tests

--- tests/testthat/test-dbi.R
+++ tests/testthat/test-dbi.R
@@ -1,11 +1,10 @@
 sc <- load_context()
 conn <- createSparkRConnection(sc)
-
-# Populating the spark context
 df <- data.frame(a = 1:3, b = c("a", "b", "c"))
-sdf <- SparkR::as.DataFrame(df)
-SparkR::saveAsTable(sdf, "testTable")
 
-# dbSendQuery should produce a SparkResult object
-res <- dbSendQuery(conn, "SELECT * FROM testTable")
-expect_identical(class(res), "SparkResult")
+test_that("dbSendQuery produce a SparkResult object", {
+    sdf <- SparkR::as.DataFrame(df)
+    SparkR::saveAsTable(sdf, "testTable", overwrite=T)
+    res <- dbSendQuery(conn, "SELECT * FROM testTable")
+    expect_identical(class(res), "SparkResult")
+})
