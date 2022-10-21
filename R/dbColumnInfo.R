081825f3b0767080af50e6e560ddd4200a704863
Ali <ali.bellamine@aphp.fr>
2022-10-21 10:36:02 +0200

    Update dbColumnInfo output type

--- R/dbColumnInfo.R
+++ R/dbColumnInfo.R
@@ -3,14 +3,14 @@ setMethod("dbColumnInfo", "SparkRResult", function(res, ...) {
     # We get the R type by collecting a row
 
     sdf <- SparkR::sql(res@state$statement)
-    columns <- SparkR::columns(sdf)
-    columns_sql_type <- sapply(SparkR::dtypes(sdf), function(x) { x[2] })
-    columns_type <- sapply(
+    columns <- as.character(SparkR::columns(sdf))
+    columns_sql_type <- as.character(sapply(SparkR::dtypes(sdf), function(x) { x[2] }))
+    columns_type <- as.character(sapply(
         SparkR::collect(
             SparkR::limit(sdf, 1)
         ), 
         class
-    )
+    ))
 
     data.frame(name=columns, type=columns_type, sql.type=columns_sql_type)
 })
